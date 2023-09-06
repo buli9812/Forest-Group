960ba621acac0137edf80c8259784ac83c00c2eb
Bartosz <lozio12345@wp.pl>
2023-09-06 23:18:46 +0200

    feat: add scroll spy to burger menu
    Added scroll spy to burger menu so it cheanges its color depends
    on background to allways have a good contrast

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
+//burger
 const body = document.body;
 const nav = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.burger-menu');
 const navItem = document.querySelectorAll('.nav-mobile__item');
+//time
+const footerYear = document.querySelector('.footer__year');
+//bars color
+const navBtnBars = document.querySelector('.fa-bars');
+const allSections = document.querySelectorAll('.section');
 
 // burger menu
 const showMobileNav = () => {
@@ -21,11 +27,25 @@ navBtn.addEventListener('click', showMobileNav);
 
 // get current year
 
-const footerYear = document.querySelector('.footer__year');
-
 const currentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 
 currentYear();
+
+// mobile burger bars color
+
+const burgerBarsColor = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 45) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 45) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+window.addEventListener('scroll', burgerBarsColor);
