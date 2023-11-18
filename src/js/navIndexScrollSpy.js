7453f2ce2ac89fcc5a9cd2c3f70faf925c2ef507
Bartosz <lozio12345@wp.pl>
2023-11-18 23:12:53 +0100

    last

--- src/js/navIndexScrollSpy.js
+++ src/js/navIndexScrollSpy.js
@@ -19,16 +19,48 @@ const scrollSpy = () => {
 			navLiAboutus.classList.add('nav-activeblock');
 		} else if (!section.classList.contains('scrollspy-aboutus') && section.offsetTop <= currentSection + 90) {
 			navLiAboutus.classList.remove('nav-activeblock');
+		} else if (navLiOffer.classList.contains('nav-activeblock')) {
+			navLiAboutus.classList.remove('nav-activeblock');
 		}
 	});
 	allSections.forEach(section => {
-		if (section.classList.contains('scrollspy-offer') && section.offsetTop <= currentSection + 90) {
+		if (section.classList.contains('scrollspy-offer') && section.offsetTop <= currentSection + 200) {
 			navLiOffer.classList.add('nav-activeblock');
-		} else if (!section.classList.contains('scrollspy-offer') && section.offsetTop <= currentSection + 90) {
+		} else if (!section.classList.contains('scrollspy-offer') && section.offsetTop <= currentSection + 200) {
 			navLiOffer.classList.remove('nav-activeblock');
 		}
 	});
+
+	allSections.forEach(section => {
+		if (section.classList.contains('contact-section')) {
+			navLiContact.classList.add('nav-activeblock');
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
+	allSections.forEach(section => {
+		if (section.classList.contains('contact-section')) {
+			navLiContact.classList.add('nav-activeblock');
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
+	allSections.forEach(section => {
+		if (section.classList.contains('offer-section')) {
+			navLiOffer.classList.add('nav-activeblock');
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
 };
+
+const scrollToSection = sectionId => {
+	let section = document.getElementById(sectionId);
+	if (section) {
+		window.scrollTo({
+			top: section.offsetTop - 90,
+			behavior: 'smooth',
+		});
+	}
+};
+
 
 window.addEventListener('load', scrollSpy);
 window.addEventListener('scroll', scrollSpy);
