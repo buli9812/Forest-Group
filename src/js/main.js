af02b23b66e606724ccf021ede59573e422bbeaa
Bartosz <lozio12345@wp.pl>
2023-12-04 14:58:42 +0100

    fix: typos in code and delete files

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
-//burger menu
+// menu
 const body = document.body;
 const html = document.documentElement;
 const nav = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.burger-menu');
+const navClose = document.querySelector('.burger-close');
 const navItem = document.querySelectorAll('.nav-mobile__item');
 const navBtnBars = document.querySelector('.fa-bars');
 const allSections = document.querySelectorAll('.section');
+const navLiHome = document.querySelector('.nav-home');
+const navLiAboutus = document.querySelector('.nav-aboutus');
+const navLiOffer = document.querySelector('.nav-offer');
+const navLiContact = document.querySelector('.nav-contact');
 //get current year footer
 const footerYear = document.querySelector('.footer__year');
 // contact form from mmcschool.pl
@@ -26,14 +31,19 @@ const showMobileNav = () => {
 	});
 };
 
-// get current year footer
-
-const currentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.innerText = year;
+const closeMobileNav = () => {
+	nav.classList.remove('nav-mobile--active');
+	body.classList.toggle('overf-hidden');
+	navBtn.classList.toggle('dpnone');
 };
 
-currentYear();
+//set body height
+
+const setHeight = () => {
+	let heightFromTop = window.scrollY;
+	let styleRule = 'top:' + '-' + heightFromTop + 'px;';
+	body.style.cssText = styleRule;
+};
 
 // mobile burger bars color
 
@@ -49,17 +59,55 @@ const burgerBarsColor = () => {
 	});
 };
 
-// navbar still same height
+//scrollspy
 
-const setHeight = () => {
-	let heightFromTop = window.scrollY;
-	let styleRule = 'top:' + '-' + heightFromTop + 'px;';
-	body.style.cssText = styleRule;
+const scrollSpy = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach(section => {
+		if (section.classList.contains('scrollspy-header') && section.offsetTop <= currentSection + 90) {
+			navLiHome.classList.add('nav-activeblock');
+		} else if (!section.classList.contains('scrollspy-header') && section.offsetTop <= currentSection + 90) {
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
+	allSections.forEach(section => {
+		if (section.classList.contains('scrollspy-aboutus') && section.offsetTop <= currentSection + 90) {
+			navLiAboutus.classList.add('nav-activeblock');
+		} else if (!section.classList.contains('scrollspy-aboutus') && section.offsetTop <= currentSection + 90) {
+			navLiAboutus.classList.remove('nav-activeblock');
+		} else if (navLiOffer.classList.contains('nav-activeblock')) {
+			navLiAboutus.classList.remove('nav-activeblock');
+		}
+	});
+	allSections.forEach(section => {
+		if (section.classList.contains('scrollspy-offer') && section.offsetTop <= currentSection + 90) {
+			navLiOffer.classList.add('nav-activeblock');
+		} else if (!section.classList.contains('scrollspy-offer') && section.offsetTop <= currentSection + 90) {
+			navLiOffer.classList.remove('nav-activeblock');
+		}
+	});
+
+	allSections.forEach(section => {
+		if (section.classList.contains('contact-section')) {
+			navLiContact.classList.add('nav-activeblock');
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
+	allSections.forEach(section => {
+		if (section.classList.contains('contact-section')) {
+			navLiContact.classList.add('nav-activeblock');
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
+	allSections.forEach(section => {
+		if (section.classList.contains('offer-section')) {
+			navLiOffer.classList.add('nav-activeblock');
+			navLiHome.classList.remove('nav-activeblock');
+		}
+	});
 };
 
 // contact form from mmcschool.pl
-
-console.log(document.location.search);
 
 if (document.location.search === '?mail_status=sent') {
 	msgStatus.classList.add('success');
@@ -79,5 +127,17 @@ if (document.location.search === '?mail_status=error') {
 	}, 3000);
 }
 
+// get current year footer
+
+const currentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+currentYear();
+
 navBtn.addEventListener('click', showMobileNav);
+navClose.addEventListener('click', closeMobileNav);
 window.addEventListener('scroll', burgerBarsColor);
+window.addEventListener('load', scrollSpy);
+window.addEventListener('scroll', scrollSpy);
